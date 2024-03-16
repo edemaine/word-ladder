1b524f5b4df629f28bb78f66a1af8217dfbf3656
Erik Demaine <edemaine@mit.edu>
2024-03-15 21:38:56 -0400

    Vite setup

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import solidPlugin from 'vite-plugin-solid'
+import civetPlugin from '@danielx/civet/vite'
+
+export default defineConfig({
+  base: './',
+  plugins: [
+    civetPlugin({
+      ts: "preserve",
+      //typecheck: true,
+    }),
+    solidPlugin(),
+  ],
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: 'esnext',
+  },
+  test: {
+    environment: 'node',
+    include: 'test/*.test.civet',
+  },
+})
